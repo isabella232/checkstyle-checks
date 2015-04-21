f4998fba571ebe74d939c56a809f01badaaf6448
Ruben Dijkstra <ruben@irp.nl>
2015-04-21 22:22:08 +0200

    Add initial checkstyle-checks project
    This includes the ArrayOrder check

---
+++ src/main/java/com/querydsl/checkstyle/checks/ArrayOrderCheck.java
@@ -0,0 +1,41 @@
+package com.querydsl.checkstyle.checks;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
+import com.puppycrawl.tools.checkstyle.api.*;
+
+public class ArrayOrderCheck extends Check {
+
+    private static final int[] DEFAULT_TOKENS = {
+        TokenTypes.ANNOTATION_ARRAY_INIT,
+    };
+
+    @Override
+    @SuppressWarnings("ReturnOfCollectionOrArrayField") //checkstyle API
+    public int[] getDefaultTokens() {
+        return DEFAULT_TOKENS;
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+        List<String> identifiers = Lists.newArrayList();
+
+        for (DetailAST child = ast.getFirstChild();
+                child != null;
+                child = child.getNextSibling()) {
+
+            if (child.getType() == TokenTypes.EXPR) {
+                FullIdent identifier = FullIdent.createFullIdentBelow(child);
+                identifiers.add(identifier.getText());
+            }
+        }
+        Ordering<String> naturalOrdering = Ordering.natural();
+        boolean ordered = naturalOrdering.isOrdered(identifiers);
+        if (!ordered) {
+            log(ast, identifiers + " is not ordered correctly, "
+                    + "expected " + naturalOrdering.sortedCopy(identifiers));
+        }
+    }
+}
